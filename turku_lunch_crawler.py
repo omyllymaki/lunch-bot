07d4be5fb4e5883d9072ba6f2cc156aae4181b03
Ossi Myllymäki <omyllymaki@gmail.com>
2019-02-10 09:24:34 +0200

    Try to handle random pop up window by refressing page

--- turku_lunch_crawler.py
+++ turku_lunch_crawler.py
@@ -2,22 +2,28 @@ from typing import Dict, List
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.common.exceptions import ElementClickInterceptedException
 
 
 class TurkuLunchCrawler:
     URL = 'https://www.lounaat.info/turku'
 
     def crawl(self) -> Dict[str, List[str]]:
-        html_text = self._get_html_text()
-        self.parser = BeautifulSoup(html_text, 'html.parser')
+        self._get_html_and_initialize_parser()
         restaurant_containers = self._crawl_restaurant_containers()
-
         data = {}
         for container in restaurant_containers:
             restaurant_name = self._crawl_restaurant_name(container)
             lunch_options = self._crawl_lunch_options(container)
+            distance = self._crawl_distance(container)
             data[restaurant_name] = lunch_options
         return data
+
+    def _get_html_and_initialize_parser(self):
+        self.driver = webdriver.Firefox()
+        html_text = self._get_html_text()
+        self.driver.close()
+        self.parser = BeautifulSoup(html_text, 'html.parser')
 
     def _crawl_lunch_options(self, container):
         lunch_options = []
@@ -30,6 +36,13 @@ class TurkuLunchCrawler:
             lunch_options.append(lunch_option)
         return lunch_options
 
+    def _crawl_distance(self, container):
+        try:
+            distance = container.find('p', {'class': 'dist'}).get_text()
+        except AttributeError:
+            distance = None
+        return distance
+
     def _crawl_restaurant_name(self, container):
         try:
             restaurant_name = container.find('h3').get_text()
@@ -38,12 +51,16 @@ class TurkuLunchCrawler:
         return restaurant_name
 
     def _get_html_text(self):
-        driver = webdriver.Firefox()
-        driver.get(self.URL)
-        for k in range(5):
-            driver.find_element_by_class_name("button.showmore").click()
-        html_text = driver.page_source
-        driver.close()
+        self.driver.get(self.URL)
+        try:
+            for k in range(5):
+                self.driver.find_element_by_class_name("button.showmore").click()
+            html_text = self.driver.page_source
+        except ElementClickInterceptedException:
+            self.driver.refresh()
+            for k in range(5):
+                self.driver.find_element_by_class_name("button.showmore").click()
+            html_text = self.driver.page_source
         return html_text
 
     def _crawl_restaurant_containers(self):
