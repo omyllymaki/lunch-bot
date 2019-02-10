563283608716dbda5f6bff5b55c28502b7e0e960
Ossi Myllymäki <omyllymaki@gmail.com>
2019-02-10 08:54:42 +0200

    Refactor code

--- slack_handler.py
+++ slack_handler.py
@@ -1,10 +1,12 @@
 import logging
+import random
 import re
 import time
-from typing import Tuple, Optional, List, Any
+from typing import Tuple, Optional, List, Any, Dict
 
 from slackclient import SlackClient
 
+from exceptions import SlackClientConnectionFailed
 from turku_lunch_crawler import TurkuLunchCrawler
 
 logger = logging.getLogger(__name__)
@@ -12,28 +14,29 @@ logger = logging.getLogger(__name__)
 
 class SlackHandler:
     RTM_READ_DELAY = 1
-    GREETINGS_COMMANDS = ["moi", "moro", "terve"]
-    ALL_RESTAURANTS_COMMANDS = ["lounaat", "lounaslistat"]
-    SINGLE_RESTAURANT_COMMANDS = ["lounas"]
     MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+    COMMANDS = {
+        'greeting': ["moi", "moro", "terve"],
+        'all_lunch_options': ["lounaat", "lounaslistat"],
+        'single_lunch_option': ["lounas"],
+    }
 
     def __init__(self, token: str):
         self.bot_id = None
         self.slack_client = SlackClient(token)
+        self.crawler = TurkuLunchCrawler()
+        self._check_connection()
 
-    def start_slack_polling(self):
-        if self._is_slack_client_connected():
-            logger.info("Slack bot connected and running!")
-            self.bot_id = self._get_user_id()
+    def activate_bot(self):
+        logger.info("Slack bot running!")
+        self.bot_id = self._get_user_id()
 
-            while True:
-                slack_events = self._get_slack_events()
-                command, channel = self._parse_bot_commands(slack_events)
-                if command:
-                    self._handle_command(command, channel)
-                time.sleep(self.RTM_READ_DELAY)
-        else:
-            logger.warning("Connection failed. Exception traceback printed above.")
+        while True:
+            slack_events = self._get_slack_events()
+            command, channel = self._parse_bot_commands(slack_events)
+            if command:
+                self._handle_command(command, channel)
+            time.sleep(self.RTM_READ_DELAY)
 
     def post_message(self, channel: str, message: str) -> None:
         self.slack_client.api_call(
@@ -58,33 +61,46 @@ class SlackHandler:
 
     def _parse_direct_mention(self, message_text: str) -> Tuple[Optional[str], Optional[str]]:
         matches = re.search(self.MENTION_REGEX, message_text)
-        return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+        if matches:
+            return matches.group(1), matches.group(2).strip()
+        return None, None
 
     def _handle_command(self, command: str, channel: str) -> None:
-        response_text = f"Mitä meinaat? Kokeile vaikkapa jotain näistä: {self.ALL_RESTAURANTS_COMMANDS}"
-        if self._is_item_in_list(command, self.GREETINGS_COMMANDS):
-            response_text = "Morjestaaa!"
-        if self._is_item_in_list(command, self.ALL_RESTAURANTS_COMMANDS):
-            crawler = TurkuLunchCrawler()
-            data = crawler.crawl()
-            response_text = self._format_message_to_slack(data)
-        if self._is_item_in_list(command, self.SINGLE_RESTAURANT_COMMANDS):
-            restaurant = ' '.join(command.split()[1:])
-            crawler = TurkuLunchCrawler()
-            data = crawler.crawl()
-            data_for_restaurant = data.get(restaurant, ['Ravintolalle ei löydy tietoja'])
-            response_text = self._format_message_to_slack({restaurant: data_for_restaurant})
+        response_text = f"Mitä meinaat? Kokeile jotain näistä: {self.COMMANDS}"
+        first_word = command.split()[0]
+        if first_word in self.COMMANDS['greeting']:
+            response_text = self._handle_greeting()
+        if first_word in self.COMMANDS['all_lunch_options']:
+            response_text = self._handle_all_lunch_options()
+        if first_word in self.COMMANDS['single_lunch_option']:
+            response_text = self._handle_single_lunch_option(command)
         self.post_message(channel=channel, message=response_text)
+
+    def _handle_greeting(self):
+        options = ['Morjestaa', 'Moro', 'Hei']
+        return random.choice(options)
+
+    def _handle_single_lunch_option(self, command: str) -> str:
+        restaurant = ' '.join(command.split()[1:])
+        data = self.crawler.crawl()
+        data_for_restaurant = data.get(restaurant, ['Ravintolalle ei löydy tietoja'])
+        response_text = self._format_message_to_slack({restaurant: data_for_restaurant})
+        return response_text
+
+    def _handle_all_lunch_options(self) -> str:
+        data = self.crawler.crawl()
+        response_text = self._format_message_to_slack(data)
+        return response_text
 
     def _is_message_to_bot(self, user_id: str) -> bool:
         return user_id == self.bot_id
 
-    def _is_slack_client_connected(self):
-        return self.slack_client.rtm_connect(with_team_state=False)
-
-    @staticmethod
-    def _is_item_in_list(target_item: str, item_list: List[str]) -> bool:
-        return any([target_item.startswith(item) for item in item_list])
+    def _check_connection(self):
+        if self.slack_client.rtm_connect(with_team_state=False):
+            logger.info("Slack bot connected!")
+        else:
+            logger.error("Slack bot connection failed!")
+            raise SlackClientConnectionFailed
 
     @staticmethod
     def _format_message_to_slack(data: Any) -> str:
--- test_bot.py
+++ test_bot.py
@@ -1,13 +1,14 @@
+import logging
+
 from config import SLACK_BOT_TOKEN
 from slack_handler import SlackHandler
-import logging
 
 logging.basicConfig(level=logging.INFO)
 
 
 def main():
     slack_handler = SlackHandler(SLACK_BOT_TOKEN)
-    slack_handler.start_slack_polling()
+    slack_handler.activate_bot()
 
 
 if __name__ == "__main__":
